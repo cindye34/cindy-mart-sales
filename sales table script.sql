b7ffa04218296d15d027abb11f6894485537a743
cindye34 <cindyessuman34@gmail.com>
2024-04-16 17:55:10 +0000

    first commit

---
+++ sales table script.sql
@@ -0,0 +1,22 @@
+-- CREATE DATABASE Cindy
+USE Cindymart;
+
+CREATE TABLE Sales( 
+Invoice_id VARCHAR (30) NOT NULL PRIMARY KEY,
+Branch VARCHAR(5)NOT NULL,
+City VARCHAR (30)NOT NULL,
+Customer_type VARCHAR(30) NOT NULL,
+Gender Varchar(10) NOT NULL,
+Product_line VARCHAR(100) NOT NULL,
+Unit_price Decimal(10,2) NOT NULL,
+Quantity INT NOT NULL,
+VAT Float(6,4) NOT NULL,
+Total DECIMAL(12,4)NOT NULL,
+Date DATETIME NOT NULL,
+Time TIME NOT NULL,
+Payment_method VARCHAR(15)NOT NULL,
+Cogs DECIMAL(10,2)NOT NULL,
+Gross_margin_pct FLOAT(11,9),
+Gross_income DECIMAL(12,4),
+Ratings FLOAT(2,1)
+);
